1b50eee86df33fc37eeccb592375423cc418e973
Mulia Nasution <mul14@users.noreply.github.com>
2024-01-18 14:16:50 +0700

    Initialize

---
+++ src/lib/navigation.js
@@ -0,0 +1,27 @@
+export const navigation = [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'Getting started', href: '/' },
+      { title: 'Installation', href: '/docs/installation' },
+    ],
+  },
+  {
+    title: 'Core Concept',
+    links: [
+      { title: 'Understanding Architecture', href: '/docs/architecture' },
+      { title: 'Project Structure', href: '/docs/structure' },
+    ],
+  },
+  {
+    title: 'Building Your Application',
+    links: [
+      { title: 'Configuration', href: '/docs/config' },
+      { title: 'Routing', href: '/docs/routing' },
+      { title: 'Database Migration', href: '/docs/db-migration' },
+      { title: 'Data Fetching', href: '/docs/data-fetching' },
+      { title: 'Command Line', href: '/docs/cli' },
+      { title: 'Deploying', href: '/docs/deploy' },
+    ],
+  },
+]
