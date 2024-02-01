98e85c3f82bff3b43b7dc4ada020e196928f879c
Mulia Nasution <mul14@users.noreply.github.com>
2024-02-01 18:45:13 +0700

    Deploy docs

---
+++ assets/docs_cli.md.qkTZN5Fu.js
@@ -0,0 +1,15 @@
+import{_ as a,c as n,o as s,V as e}from"./chunks/framework.IkzmnC3X.js";const u=JSON.parse('{"title":"Command Line","description":"","frontmatter":{"title":"Command Line"},"headers":[],"relativePath":"docs/cli.md","filePath":"docs/cli.md"}'),p={name:"docs/cli.md"},o=e(`<h1 id="command-line" tabindex="-1">Command Line <a class="header-anchor" href="#command-line" aria-label="Permalink to &quot;Command Line&quot;">​</a></h1><p>Raiden offers a Command Line Interface (CLI) tool to interact with the code and service from the command line.</p><p>The CLI typically comes with a built-in help command (<code>raiden --help</code>) which allows users to access information about available commands, their usage, and options.</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>Usage:</span></span>
+<span class="line"><span>  raiden [command]</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Available Commands:</span></span>
+<span class="line"><span>  completion  Generate the autocompletion script for the specified shell</span></span>
+<span class="line"><span>  generate    Generate application resource</span></span>
+<span class="line"><span>  help        Help about any command</span></span>
+<span class="line"><span>  run         Deploy and run application</span></span>
+<span class="line"><span>  start       Start new app</span></span>
+<span class="line"><span>  version     Show application information</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Flags:</span></span>
+<span class="line"><span>  -h, --help   help for raiden</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Use &quot;raiden [command] --help&quot; for more information about a command.</span></span></code></pre></div>`,4),i=[o];function t(l,c,r,d,m,h){return s(),n("div",null,i)}const f=a(p,[["render",t]]);export{u as __pageData,f as default};
